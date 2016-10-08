35e858d7c5107b3007d887104c61b45e8ef7fca5
Ekaterina <ka-ve@mail.ru>
2016-10-08 22:09:41 +0300

    Two thirds

---
+++ vigenere.py
@@ -0,0 +1,54 @@
+plaintext=input('Слово? -')
+keyword=input('Ключ? -')
+def encrypt_vigenere(plaintext, keyword):
+    ciphertext=''
+    keyword_1=''
+    for i in range(len(plaintext)):
+        num_word=ord(plaintext[i])
+        while len(plaintext)>len(keyword_1):
+            keyword_1+=keyword
+        num_key=ord(keyword_1[i])
+        if num_key<=90:
+            num_key-=65
+        elif num_key>=97:
+            num_key-=97
+        if num_word<=90:
+            if (num_key+num_word)>90:
+                num_word-=26
+        else:
+            if (num_key+num_word)>120:
+                num_word-=26
+        ciphertext+=chr(num_word+num_key)
+    return ciphertext
+print(encrypt_vigenere(plaintext, keyword))
+ciphertext=input('Слово? -')
+keyword=input('Ключ? -')
+def decrypt_vigenere(ciphertext, keyword):
+    """
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+    plaintext=''
+    keyword_1=''
+    for i in range(len(ciphertext)):
+        num_word=ord(ciphertext[i])
+        while len(ciphertext)>len(keyword_1):
+            keyword_1+=keyword
+        num_key=ord(keyword_1[i])
+        if num_key<=90:
+            num_key-=65
+        elif num_key>=97:
+            num_key-=97
+        if num_word<=90:
+            if (num_word-num_key)<65:
+                num_word+=26
+        else:
+            if (num_word-num_key)<97:
+                num_word+=26
+        plaintext+=chr(num_word-num_key)
+    return plaintext
+print(decrypt_vigenere(ciphertext, keyword))
