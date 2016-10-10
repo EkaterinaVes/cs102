8f4942343c631b48bc0037198e4fb2565cc1d0c7
Ekaterina Vesnina <ka-ve@mail.ru>
2016-10-10 12:57:16 +0200

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,5 +1,5 @@
-plaintext=input('Слово? -')
-keyword=input('Ключ? -')
+plaintext = input('Слово? -')
+keyword = input('Ключ? -')
 def encrypt_vigenere(plaintext, keyword):
     """
     >>> encrypt_vigenere("PYTHON", "A")
@@ -9,28 +9,28 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext=''
-    keyword_1=''
+    ciphertext = ''
+    keyword_1 = ''
     for i in range(len(plaintext)):
-        num_word=ord(plaintext[i])
-        while len(plaintext)>len(keyword_1):
-            keyword_1+=keyword
-        num_key=ord(keyword_1[i])
-        if num_key<=90:
-            num_key-=65
-        elif num_key>=97:
-            num_key-=97
-        if num_word<=90:
-            if (num_key+num_word)>90:
-                num_word-=26
+        num_word = ord(plaintext[i])
+        while len(plaintext) > len(keyword_1):
+            keyword_1 += keyword
+        num_key = ord(keyword_1[i])
+        if num_key <= 90:
+            num_key -= 65
+        elif num_key >= 97:
+            num_key -= 97
+        if num_word <= 90:
+            if (num_key+num_word) > 90:
+                num_word -= 26
         else:
-            if (num_key+num_word)>120:
-                num_word-=26
-        ciphertext+=chr(num_word+num_key)
+            if (num_key+num_word) > 120:
+                num_word -= 26
+        ciphertext += chr(num_word+num_key)
     return ciphertext
 print(encrypt_vigenere(plaintext, keyword))
-ciphertext=input('Слово? -')
-keyword=input('Ключ? -')
+ciphertext = input('Слово? -')
+keyword = input('Ключ? -')
 def decrypt_vigenere(ciphertext, keyword):
     """
     >>> decrypt_vigenere("PYTHON", "A")
@@ -40,23 +40,23 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext=''
-    keyword_1=''
+    plaintext = ''
+    keyword_1 = ''
     for i in range(len(ciphertext)):
-        num_word=ord(ciphertext[i])
-        while len(ciphertext)>len(keyword_1):
-            keyword_1+=keyword
-        num_key=ord(keyword_1[i])
-        if num_key<=90:
-            num_key-=65
-        elif num_key>=97:
-            num_key-=97
-        if num_word<=90:
-            if (num_word-num_key)<65:
-                num_word+=26
+        num_word = ord(ciphertext[i])
+        while len(ciphertext) > len(keyword_1):
+            keyword_1 += keyword
+        num_key = ord(keyword_1[i])
+        if num_key <= 90:
+            num_key -= 65
+        elif num_key >= 97:
+            num_key -= 97
+        if num_word <= 90:
+            if (num_word-num_key) < 65:
+                num_word += 26
         else:
-            if (num_word-num_key)<97:
-                num_word+=26
-        plaintext+=chr(num_word-num_key)
+            if (num_word-num_key) < 97:
+                num_word += 26
+        plaintext += chr(num_word-num_key)
     return plaintext
 print(decrypt_vigenere(ciphertext, keyword))
