1e73251b314b43a3f6f0611adab8e509310daba1
Ekaterina Vesnina <ka-ve@mail.ru>
2016-10-17 13:38:42 +0200

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -15,7 +15,7 @@ def is_prime(n):
     for i in range(2, n):
         if n % i == 0:
             count += 1
-    if count == 0:
+    if count == 0 or n == 1:
         return True
     else:
         return False
